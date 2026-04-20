0af76f47acee0e58ed2b6a9b1d421201526be160
Jonathan <Jonathan.dtr@hotmail.com>
2026-04-20 21:54:15 +0100

    edited out some mistakes in  golf_masters.txt .Also finished up the starter code and polished it.I ran the code a couple of times and all seemed to work

--- golf_project.py
+++ golf_project.py
@@ -120,72 +120,42 @@ def update_cut_status(golfer_ids, player_names, cut_status):
     else:
         print("No changes made.")
 
+#------ Main Function---------
+## MOC starter code for main function US Golf Master's Leaderboard Project
 
-
-
-"""def main():
-    filename = "golf_masters.txt"
-    golfer_ids, player_names, par_score, cut_status = read_file(golfer.txt)
-    choice = ""
-while choice != "8":
-    print("\nMenu")
-    print("1. View Leaderboard")
-    print("2. Delete a player")
-    print("3. Add a new player")
-    print("4. Update cut status")
-    print("5. Placeholder for Exam")
-    print("6. Placeholder for Exam")
-    print("7. Placeholder for Exam")
-    print("8. Quit and save")
-    choice = input("Enter choice: ")
-    if choice == "1":
-
-    #display leader board
-    elif choice == "2":
-    # delete a player
-    elif choice == "3":
-    # add a new player
-    elif choice == "4":
-    # update cut status
-    elif choice == "5":
-#
-# save data and quit
-print("Data saved. Goodbye.")
-else:
-    print("Invalid choice.")
-    if __name__ == '__main__':
-=======
- #MOC starter code for main function US Golf Master's Leaderboard Project
 def main():
-    filename = "golf_masters.txt"
-    golfer_ids, player_names, par_score, cut_status = read_file(golfer.txt)
+    golfer_ids, player_names, par_score, cut_status, rounds = read_file()
     choice = ""
-while choice != "8":
-    print("\nMenu")
-    print("1. View Leaderboard")
-    print("2. Delete a player")
-    print("3. Add a new player")
-    print("4. Update cut status")
-    print("5. Placeholder for Exam")
-    print("6. Placeholder for Exam")
-    print("7. Placeholder for Exam")
-    print("8. Quit and save")
-    choice = input("Enter choice: ")
-    if choice == "1":
 
-    #display leader board
-    elif choice == "2":
-    # delete a player
-    elif choice == "3":
-    # add a new player
-    elif choice == "4":
-    # update cut status
-    elif choice == "5":
-#
-# save data and quit
-print("Data saved. Goodbye.")
-else:
-    print("Invalid choice.")
-    if __name__ == '__main__':
->>>>>>> e8c03bbda478e3723cd686f94e5796ab0962347f
-    main()"""
+    while choice != "8":
+        print("\nMenu")
+        print("1. View Leaderboard")
+        print("2. Delete a player")
+        print("3. Add a new player")
+        print("4. Update cut status")
+        print("5. Placeholder for Exam")
+        print("6. Placeholder for Exam")
+        print("7. Placeholder for Exam")
+        print("8. Quit and save")
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            view_leaderboard(golfer_ids, player_names, par_score, cut_status, rounds)
+        elif choice == "2":
+            delete_player(golfer_ids, player_names, par_score, cut_status, rounds)
+        elif choice == "3":
+            add_player(golfer_ids, player_names, par_score, cut_status, rounds)
+        elif choice == "4":
+            update_cut_status(golfer_ids, player_names, cut_status)
+        elif choice in ["5", "6", "7"]:
+            print("to do in exam.")
+        elif choice == "8":
+            save_file(golfer_ids, player_names, par_score, cut_status, rounds)
+            print("data saved. goodbye.")
+        else:
+            print("invalid choice.")
+
+if __name__ == '__main__':
+    main()
+
+
